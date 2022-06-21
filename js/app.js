0beb9a07db37408d56cf6932f8395d7abc9ce451
pap90 <38990493+pap90@users.noreply.github.com>
2022-06-20 21:47:13 -0600

    commit primero y ultimo

---
+++ js/app.js
@@ -0,0 +1,111 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'App Framework7',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/vista/',
+    	url: 'vista.html',
+    	name: 'vista',
+  		},
+		{
+		path: '/cards/',
+    	url: 'cards.html',
+    	name: 'cards',
+  		},
+		{
+		path: '/swiper/',
+    	url: 'swiper.html',
+    	name: 'swiper',
+  		},
+		{
+		path: '/about-us/',
+    	url: 'about-us.html',
+    	name: 'about-us',
+  		},
+		{
+		path: '/mision/',
+    	url: 'mision.html',
+    	name: 'mision',
+  		},
+		{
+		path: '/terminos/',
+    	url: 'terminos.html',
+    	name: 'terminos',
+  		}
+		
+	],
+	dialog: {
+		title: 'Rythmica',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#modal-login');
+		
+		app.dialog.alert('¡Bienvenido a CGC!');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnAddCart', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
